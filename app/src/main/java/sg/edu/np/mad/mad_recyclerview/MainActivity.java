7d3c86f61b0d88b64c7b20e5780e0651a7ffa609
Zong Hao Ben Chang <s10185167@connect.np.edu.sg>
2020-06-08 00:00:12 +0800

    updated

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
@@ -1,21 +1,61 @@
 package sg.edu.np.mad.mad_recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.CheckedInputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+    private List<DataModel> dataModels;
+    private TextView TaskDesc;
+    private Button TaskAdd;
+    private CheckBox check;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DataModel.TaskList taskList = new DataModel.TaskList();
+
+        TaskDesc = (EditText) findViewById(R.id.taskDesc);
+        TaskAdd = (Button) findViewById(R.id.taskAdd);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        dataModels = new ArrayList<>();
+
+
+        recyclerView.setAdapter(adapter);
+
+
+        adapter = new Adapter(taskList);
+        LinearLayoutManager taskLayoutManager = new LinearLayoutManager(this);
+
+        RecyclerView.setLayoutManager(taskLayoutManager);
+        RecyclerView.setItemAnimator(new DefaultItemAnimator());
+        RecyclerView.setAdapter(adapter);
     }
+
+
+
 
     /**
      * Upon calling this method, the keyboard will retract
